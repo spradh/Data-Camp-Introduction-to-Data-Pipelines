15cea25ad5720619285d89ce651ba2582be6afa4
Sabin <spradh@users.noreply.github.com>
2024-03-04 12:26:39 -0600

    Create 07 Loading sales data to a CSV file.py

---
+++ 02 Building ETL Piplines/07 Loading sales data to a CSV file.py
@@ -0,0 +1,13 @@
+def transform(raw_data):
+	# Find the items prices less than 25 dollars
+	return raw_data.loc[raw_data["Price Each"] < 24, ["Order ID", "Product", "Price Each", "Order Date"]]
+
+def load(clean_data):
+	# Write the data to a CSV file without the index column
+	return clean_data.to_csv("transformed_sales_data.csv", index=False)
+
+
+clean_sales_data = transform(raw_sales_data)
+
+# Call the load function on the cleaned DataFrame
+load(clean_sales_data)
